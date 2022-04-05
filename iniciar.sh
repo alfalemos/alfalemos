14ed5f7bd93f5c60727863b8a8127ed5b8e9f29d
alfalemos <101687026+alfalemos@users.noreply.github.com>
2022-04-05 03:12:05 -0300

    Update iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -1,16 +1,57 @@
-Í### Hi there 👋
+clear
 
-<!--
-**alfalemos/alfalemos** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+if [ ! -d "bot" ]; then
+	mkdir bot
+fi
 
-Here are some ideas to get you started:
+cd bot
 
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+if [ -e "dadosBot.ini" ] ; then
+
+	screen -X -S bot quit > /dev/null
+	screen -dmS bot php bot.php
+	echo "Bot foi reiniciado e está executano em segundo plano"
+
+else
+
+echo "Instalando dependencias, aguarde..."
+
+#add-apt-repository ppa:ondrej/php > /dev/null 2>&1
+
+apt-get update > /dev/null 2>&1
+apt-get upgrade -y > /dev/null 2>&1
+apt-get install php -y > /dev/null 2>&1
+apt-get install php-redis -y > /dev/null 2>&1
+apt-get install php-curl -y > /dev/null 2>&1
+apt-get install php5 -y > /dev/null 2>&1
+apt-get install php5-redis -y > /dev/null 2>&1
+apt-get install php5-curl -y > /dev/null 2>&1
+apt-get install redis-server -y > /dev/null 2>&1
+apt-get install redis -y > /dev/null 2>&1
+apt-get install screen -y > /dev/null 2>&1
+apt-get install zip -y > /dev/null 2>&1
+
+wget https://www.dropbox.com/s/j9bpk6m27egkwkp/gerarusuario-sshplus.sh?dl=0 -O gerarusuario.sh; chmod +x gerarusuario.sh > /dev/null
+
+wget https://github.com/alfalemos/alfalemos/raw/main/%25admysshbot.zip -O bot.zip && unzip bot.zip > /dev/null
+
+rm dadosBot.ini > /dev/null
+
+clear
+
+ip=$(wget -qO- ipv4.icanhazip.com/)
+
+echo "Digite o token do seu bot:"
+read token
+clear
+echo "ip=$ip
+token=$token
+limite=300" >> dadosBot.ini
+
+screen -dmS bot php bot.php
+
+rm bot.zip
+
+echo "Pronto, vai la testar essa bagaça"
+
+fi
